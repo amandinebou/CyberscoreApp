9d79fdbbe2924ea387f7e8e72e3212fdf4a16797
Amandine Bouguessa <amandine.bouguessa1@etu.univ-lorraine.fr>
2021-05-03 15:32:08 +0200

    rajout liste données

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import { Text, View, TextInput, ScrollView, TouchableOpacity, FlatList, Keyboard, StyleSheet, Image } from 'react-native';
 
-
+import DevicesItem from '../components/DevicesItems.js';
+import devices from '../Data/DevicesData'
 import { SearchBar } from 'react-native-elements';
 
 export default class SearchScreen extends React.Component {
@@ -11,7 +12,10 @@ export default class SearchScreen extends React.Component {
   };
   updateSearch = (search: any) => { this.setState({ search });
   };
+
+  
   render() {
+
   const { search } = this.state;
   return (
     <View style={styles.container}>
@@ -20,9 +24,11 @@ export default class SearchScreen extends React.Component {
         placeholder="Type Here to Search..." onChangeText={this.updateSearch} value={search}
       />
   
-      <ScrollView style = {styles.scrollContainer}>
-  
-      </ScrollView>
+      <FlatList style={styles.list}
+      data = {devices}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={({item}) => <DevicesItem device={item}/>}
+      />
   
     </View>
   );
@@ -36,6 +42,9 @@ export default class SearchScreen extends React.Component {
     scrollContainer: {
       flex: 1,
       marginBottom: 100,
+    },
+    list: {
+      flex: 1,
     },
   });
   
